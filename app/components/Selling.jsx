39652e08985afcbfff79e7c02ff018e804b9b359
Manan Batra <83981142+MaNaNBaTrA@users.noreply.github.com>
2025-06-08 21:13:24 +0530

    Updated Selling Styling

--- app/components/Selling.jsx
+++ app/components/Selling.jsx
@@ -4,9 +4,8 @@ import Image from 'next/image';
 const Selling = () => {
     return (
         <div className='w-screen h-full flex items-center justify-center py-8 px-4 sm:px-8 font-poppins flex-col gap-12'>
-            {/* Icons Row */}
-            <div className='w-screen  flex items-center justify-center'>
-                <div className='w-full flex-wrap flex sm:justify-between max-w-5xl gap-y-8 gap-x-6'>
+            <div className='w-screen flex items-center justify-center'>
+                <div className='w-full flex-wrap flex justify-center max-w-5xl gap-y-8 gap-x-6'>
                     <div className='flex flex-col justify-center items-center gap-3 w-[120px]'>
                         <Image
                             src="/Svg/S1.svg"
@@ -25,7 +24,7 @@ const Selling = () => {
                         />
                         <div className='font-semibold text-center'>Drone Crash Warranty</div>
                     </div>
-                    <div className='flex flex-col justify-center items-center gap-3  w-[120px]'>
+                    <div className='flex flex-col justify-center items-center gap-3 w-[120px]'>
                         <Image
                             src="/Svg/S3.svg"
                             alt="Free Consultation"
@@ -34,7 +33,7 @@ const Selling = () => {
                         />
                         <div className='font-semibold text-center'>Free Consultation</div>
                     </div>
-                    <div className='flex flex-col justify-center items-center gap-3  w-[120px]'>
+                    <div className='flex flex-col justify-center items-center gap-3 w-[120px]'>
                         <Image
                             src="/Svg/S4.svg"
                             alt="Secure payment"
@@ -43,7 +42,7 @@ const Selling = () => {
                         />
                         <div className='font-semibold text-center'>Secure Payment</div>
                     </div>
-                    <div className='flex flex-col justify-center items-center gap-3  w-[120px]'>
+                    <div className='flex flex-col justify-center items-center gap-3 w-[120px]'>
                         <Image
                             src="/Svg/S5.svg"
                             alt="Easy Accessibility"
